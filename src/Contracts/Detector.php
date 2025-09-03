844c2ce6ca23be329dbf73ae9511643559dc56ed
Yukata Roommate <take.yama0519@gmail.com>
2025-09-03 16:20:25 +0900

    First Publish

--- src/Contracts/Detector.php
+++ src/Contracts/Detector.php
@@ -9,4 +9,46 @@ namespace Sentinel\Contracts;
  *
  * @package Sentinel\Contracts
  */
-interface Detector {}
+interface Detector
+{
+    /*----------------------------------------*
+     * Name
+     *----------------------------------------*/
+
+    /**
+     * Get detector name
+     *
+     * @return string
+     */
+    public function name(): string;
+    
+    /*----------------------------------------*
+     * Detect
+     *----------------------------------------*/
+
+    /**
+     * Detect issues in directory
+     *
+     * @param string $directory
+     * @return bool
+     */
+    public function detect(string $directory): bool;
+    
+    /*----------------------------------------*
+     * Statistics
+     *----------------------------------------*/
+
+    /**
+     * Get detected issues
+     *
+     * @return array<\Sentinel\Contracts\Issue>
+     */
+    public function issues(): array;
+
+    /**
+     * Get number of detected files
+     *
+     * @return int
+     */
+    public function files(): int;
+}
--- src/Detector.php
+++ src/Detector.php
@@ -4,11 +4,28 @@ declare(strict_types=1);
 
 namespace Sentinel;
 
+use Sentinel\Contracts\Detector as DetectorContract;
+
 /**
  * Detector
  *
  * @package Sentinel
  */
-abstract class Detector
+abstract class Detector implements DetectorContract
 {
+    /*----------------------------------------*
+     * Name
+     *----------------------------------------*/
+    
+    /*----------------------------------------*
+     * Detect
+     *----------------------------------------*/
+    
+    /*----------------------------------------*
+     * Statistics
+     *----------------------------------------*/
+
+    /*----------------------------------------*
+     * Gitignore
+     *----------------------------------------*/
 }
