17671cbcddb62a555aca305e3d14b1bb582cd941
Yukata Roommate <take.yama0519@gmail.com>
2025-09-11 08:34:51 +0900

    First Publish

--- src/Contracts/Detector.php
+++ src/Contracts/Detector.php
@@ -21,7 +21,7 @@ interface Detector
      * @return string
      */
     public function name(): string;
-    
+
     /*----------------------------------------*
      * Detect
      *----------------------------------------*/
@@ -33,7 +33,90 @@ interface Detector
      * @return bool
      */
     public function detect(string $directory): bool;
-    
+
+    /*----------------------------------------*
+     * Exclude Directories
+     *----------------------------------------*/
+
+    /**
+     * Get exclude directories
+     *
+     * @return array<string>
+     */
+    public function excludeDirectories(): array;
+
+    /**
+     * Set exclude directories
+     *
+     * @param array<string> $directories
+     * @return static
+     */
+    public function setExcludeDirectories(array $directories): static;
+
+    /**
+     * Add exclude directory
+     *
+     * @param string $directory
+     * @return static
+     */
+    public function addExcludeDirectory(string $directory): static;
+
+    /**
+     * Remove exclude directory
+     *
+     * @param string $directory
+     * @return static
+     */
+    public function removeExcludeDirectory(string $directory): static;
+
+    /*----------------------------------------*
+     * Gitignore
+     *----------------------------------------*/
+
+    /**
+     * Set whether to use gitignore
+     *
+     * @param bool $useGitignore
+     * @return static
+     */
+    public function setUseGitignore(bool $useGitignore): static;
+
+    /**
+     * Set true to use gitignore
+     *
+     * @return static
+     */
+    public function useGitignore(): static;
+
+    /**
+     * Set false to not use gitignore
+     *
+     * @return static
+     */
+    public function notUseGitignore(): static;
+
+    /**
+     * Set whether to preload all gitignores
+     *
+     * @param bool $preloadGitignores
+     * @return static
+     */
+    public function setPreloadGitignores(bool $preloadGitignores): static;
+
+    /**
+     * Set true to preload all gitignores
+     *
+     * @return static
+     */
+    public function preloadGitignores(): static;
+
+    /**
+     * Set false to not preload all gitignores
+     *
+     * @return static
+     */
+    public function notPreloadGitignores(): static;
+
     /*----------------------------------------*
      * Statistics
      *----------------------------------------*/
